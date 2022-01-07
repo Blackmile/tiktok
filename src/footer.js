23dc485af3e25a971910bd13751ea10202937bea
Stanley Eze <stanley.ezeifeanyi@gmail.com>
2022-01-07 05:05:18 +0100

    add ticker and spinner

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import './footer.css';
+import Ticker from 'react-ticker';
+import Mus from './vids/apple-music-logo-circle-png-28.png'
+
 
 function footer() {
     return (
@@ -8,9 +11,19 @@ function footer() {
             <div className="foots_attri">
                 <h4>@stanley_eis</h4>
                 <p>this is a footer and thats all you should know :)</p>
-                <div className="Icon">
+                <div className="ticker">
                     <MusicNoteIcon className="Icon_icon" />
+                    <Ticker mode='smooth' >
+                        {({ index }) => (
+                            <>
+                                <p>my emotions</p>
+                            </>
+                        )}
+                    </Ticker>
                 </div>
+            </div>
+            <div className='spinner'>
+                <img src={Mus} />
             </div>
         </div>
     )
