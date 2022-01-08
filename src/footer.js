664f1271527de9c2cbed14480afcd39910693da7
Stanley Eze <stanley.ezeifeanyi@gmail.com>
2022-01-08 13:40:13 +0100

    refactor

--- src/footer.js
+++ src/footer.js
@@ -5,25 +5,25 @@ import Ticker from 'react-ticker';
 import Mus from './vids/apple-music-logo-circle-png-28.png'
 
 
-function footer() {
+function footer({ channel, description, song }) {
     return (
         <div className="foots">
             <div className="foots_attri">
-                <h4>@stanley_eis</h4>
-                <p>this is a footer and thats all you should know :)</p>
+                <h4>@{channel}</h4>
+                <p>{description}</p>
                 <div className="ticker">
                     <MusicNoteIcon className="Icon_icon" />
                     <Ticker mode='smooth' >
                         {({ index }) => (
                             <>
-                                <p>my emotions</p>
+                                <p>{song}</p>
                             </>
                         )}
                     </Ticker>
                 </div>
             </div>
             <div className='spinner'>
-                <img src={Mus} />
+                <img src={Mus} alt=''/>
             </div>
         </div>
     )
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sidebar.css';
+import { Favorite, FavoriteBorder, Message, Share } from '@material-ui/icons';
 
-function sidebar() {
+function Sidebar({ likes, shares, messages }) {
+
+    const [liked, setLiked] = useState(false)
+
     return (
-        <div>
-            
+        <div className='videoSidebar'>
+            <div className='videoSidebar_button'>
+                {liked ? (
+                    <Favorite fontsize='large' />
+                ):(
+                    <FavoriteBorder fontSize='large' onClick={(e) => setLiked(true)} />
+                )}
+                <p>{liked ? likes += 1 : likes}</p>
+            </div>
+            <div className='videoSidebar_button'>
+                <Message fontSize='large' />
+                <p>{messages}</p>
+            </div>
+            <div className='videoSidebar_button'>
+                <Share fontSize='large' />
+                <p>{shares}</p>
+            </div>
         </div>
     )
 }
 
-export default sidebar
+export default Sidebar
