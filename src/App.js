a85dfabbd9ebaabd886d3ed4c0eafdb89a84185d
Stanley Eze <stanley.ezeifeanyi@gmail.com>
2022-01-08 13:39:20 +0100

    useEffect to make aysnc calls

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import Video from './Video';  
+import React, { useEffect, useState } from 'react';
+import Video from './Video';
 import './App.css';
+import axios from './axios';
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+  }, []);
+
+  console.log(videos)
+
   return (
     <div className="app">
       <div className="app_video">
-        <Video />
-        <Video />
-        <Video />
+        {videos.map(({ url, channel, description, song, likes, messages, shares }) => (
+          <Video 
+            url={url}
+            channel={channel}
+            description={description}
+            song={song}
+            likes={likes}
+            shares={shares}
+            messages={messages} 
+          />
+        ))}
       </div>
     </div>
   );
